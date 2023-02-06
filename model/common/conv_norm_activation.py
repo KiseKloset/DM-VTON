a76d40e2c60a012c1a20313b2a8883fbf8111bd4
Zero-nnkn <nnkhoinguyen9@gmail.com>
2023-02-06 22:10:03 +0700

    ✨ feat: Overwrite torch ConvNormActivation

---
+++ model/common/conv_norm_activation.py
@@ -0,0 +1,203 @@
+"""
+Standard Conv block: Conv - Norm - Activation: https://pytorch.org/vision/main/ops.html
+Edit from: https://github.com/pytorch/vision/blob/main/torchvision/ops/misc.py
+"""
+
+import collections
+from itertools import repeat
+from typing import Any, Callable, Optional, Sequence, Union
+import warnings
+
+import torch.nn as nn
+
+
+def _make_ntuple(x: Any, n: int) -> tuple[Any, ...]:
+    """
+    Make n-tuple from input x. If x is an iterable, then we just convert it to tuple.
+    Otherwise, we will make a tuple of length n, all with value of x.
+    Reference: https://github.com/pytorch/pytorch/blob/master/torch/nn/modules/utils.py#L8
+    Args:
+        x (Any): input value
+        n (int): length of the resulting tuple
+    """
+    if isinstance(x, collections.abc.Iterable):
+        return tuple(x)
+    return tuple(repeat(x, n))
+
+
+class CustomeConvNormActivation(nn.Sequential):
+    """
+    Configurable block used for Convolution-Normalization-Activation blocks.
+    Edit: Support params for activation function
+    Reference: https://github.com/pytorch/vision/blob/main/torchvision/ops/misc.py
+    Args:
+        in_channels (int): Number of channels in the input image
+        out_channels (int): Number of channels produced by the Convolution-Normalization-Activation block
+        kernel_size: (int, optional): Size of the convolving kernel. Default: 3
+        stride (int, optional): Stride of the convolution. Default: 1
+        padding (int, tuple or str, optional): Padding added to all four sides of the input. Default: None, in which case it will be calculated as ``padding = (kernel_size - 1) // 2 * dilation``
+        groups (int, optional): Number of blocked connections from input channels to output channels. Default: 1
+        norm_layer (Callable[..., torch.nn.Module], optional): Norm layer that will be stacked on top of the convolution layer. If ``None`` this layer won't be used. Default: ``torch.nn.BatchNorm2d``
+        activation_layer (Callable[..., torch.nn.Module], optional): Activation function which will be stacked on top of the normalization layer (if not None), otherwise on top of the conv layer. If ``None`` this layer won't be used. Default: ``torch.nn.ReLU``
+        activation_params (dict, optional): params for activation function
+        dilation (int): Spacing between kernel elements. Default: 1
+        bias (bool, optional): Whether to use bias in the convolution layer. By default, biases are included if ``norm_layer is None``.
+        conv_layer (Callable[..., torch.nn.Module]): Convolution layer
+    """
+    def __init__(
+        self,
+        in_channels: int,
+        out_channels: int,
+        kernel_size: Union[int, tuple[int, ...]] = 3,
+        stride: Union[int, tuple[int, ...]] = 1,
+        padding: Optional[Union[int, tuple[int, ...], str]] = None,
+        groups: int = 1,
+        norm_layer: Optional[Callable[..., nn.Module]] = nn.BatchNorm2d,
+        activation_layer: Optional[Callable[..., nn.Module]] = nn.ReLU,
+        activation_params: Optional[dict] = {},
+        dilation: Union[int, tuple[int, ...]] = 1,
+        bias: Optional[bool] = None,
+        conv_layer: Callable[..., nn.Module] = nn.Conv2d,
+    ) -> None:
+
+        if padding is None:
+            if isinstance(kernel_size, int) and isinstance(dilation, int):
+                padding = (kernel_size - 1) // 2 * dilation
+            else:
+                _conv_dim = len(kernel_size) if isinstance(kernel_size, Sequence) else len(dilation)
+                kernel_size = _make_ntuple(kernel_size, _conv_dim)
+                dilation = _make_ntuple(dilation, _conv_dim)
+                padding = tuple((kernel_size[i] - 1) // 2 * dilation[i] for i in range(_conv_dim))
+        if bias is None:
+            bias = norm_layer is None
+
+        layers = [
+            conv_layer(
+                in_channels,
+                out_channels,
+                kernel_size,
+                stride,
+                padding,
+                dilation=dilation,
+                groups=groups,
+                bias=bias,
+            )
+        ]
+
+        if norm_layer is not None:
+            layers.append(norm_layer(out_channels))
+
+        if activation_layer is not None:
+            layers.append(activation_layer(**activation_params))
+
+        super().__init__(*layers)
+        self.out_channels = out_channels
+
+        if self.__class__ == CustomeConvNormActivation:
+            warnings.warn(
+                "Don't use ConvNormActivation directly, please use Conv2dNormActivation and Conv3dNormActivation instead."
+            )
+
+
+class CustomeConv2dNormActivation(CustomeConvNormActivation):
+    """
+    Configurable block used for Convolution2d-Normalization-Activation blocks.
+    Edit: Support params for activation function
+    Reference: https://github.com/pytorch/vision/blob/main/torchvision/ops/misc.py
+    Args:
+        in_channels (int): Number of channels in the input image
+        out_channels (int): Number of channels produced by the Convolution-Normalization-Activation block
+        kernel_size: (int, optional): Size of the convolving kernel. Default: 3
+        stride (int, optional): Stride of the convolution. Default: 1
+        padding (int, tuple or str, optional): Padding added to all four sides of the input. Default: None, in which case it will be calculated as ``padding = (kernel_size - 1) // 2 * dilation``
+        groups (int, optional): Number of blocked connections from input channels to output channels. Default: 1
+        norm_layer (Callable[..., torch.nn.Module], optional): Norm layer that will be stacked on top of the convolution layer. If ``None`` this layer won't be used. Default: ``torch.nn.BatchNorm2d``
+        activation_layer (Callable[..., torch.nn.Module], optional): Activation function which will be stacked on top of the normalization layer (if not None), otherwise on top of the conv layer. If ``None`` this layer won't be used. Default: ``torch.nn.ReLU``
+        dilation (int): Spacing between kernel elements. Default: 1
+        inplace (bool): Parameter for the activation layer, which can optionally do the operation in-place. Default ``True``
+        bias (bool, optional): Whether to use bias in the convolution layer. By default, biases are included if ``norm_layer is None``.
+    """
+
+    def __init__(
+        self,
+        in_channels: int,
+        out_channels: int,
+        kernel_size: Union[int, tuple[int, int]] = 3,
+        stride: Union[int, tuple[int, int]] = 1,
+        padding: Optional[Union[int, tuple[int, int], str]] = None,
+        groups: int = 1,
+        norm_layer: Optional[Callable[..., nn.Module]] = nn.BatchNorm2d,
+        activation_layer: Optional[Callable[..., nn.Module]] = nn.ReLU,
+        activation_params: Optional[dict] = {},
+        dilation: Union[int, tuple[int, int]] = 1,
+        inplace: Optional[bool] = True,
+        bias: Optional[bool] = None,
+    ) -> None:
+
+        super().__init__(
+            in_channels,
+            out_channels,
+            kernel_size,
+            stride,
+            padding,
+            groups,
+            norm_layer,
+            activation_layer,
+            activation_params,
+            dilation,
+            inplace,
+            bias,
+            nn.Conv2d,
+        )
+
+
+class CustomeConvTranspose2dNormActivation(CustomeConvNormActivation):
+    """
+    Configurable block used for ConvTranspose2d-Normalization-Activation blocks.
+    Edit: Support params for activation function
+    Reference: https://github.com/pytorch/vision/blob/main/torchvision/ops/misc.py
+    Args:
+        in_channels (int): Number of channels in the input image
+        out_channels (int): Number of channels produced by the Convolution-Normalization-Activation block
+        kernel_size: (int, optional): Size of the convolving kernel. Default: 3
+        stride (int, optional): Stride of the convolution. Default: 1
+        padding (int, tuple or str, optional): Padding added to all four sides of the input. Default: None, in which case it will be calculated as ``padding = (kernel_size - 1) // 2 * dilation``
+        groups (int, optional): Number of blocked connections from input channels to output channels. Default: 1
+        norm_layer (Callable[..., torch.nn.Module], optional): Norm layer that will be stacked on top of the convolution layer. If ``None`` this layer won't be used. Default: ``torch.nn.BatchNorm2d``
+        activation_layer (Callable[..., torch.nn.Module], optional): Activation function which will be stacked on top of the normalization layer (if not None), otherwise on top of the conv layer. If ``None`` this layer won't be used. Default: ``torch.nn.ReLU``
+        dilation (int): Spacing between kernel elements. Default: 1
+        inplace (bool): Parameter for the activation layer, which can optionally do the operation in-place. Default ``True``
+        bias (bool, optional): Whether to use bias in the convolution layer. By default, biases are included if ``norm_layer is None``.
+    """
+
+    def __init__(
+        self,
+        in_channels: int,
+        out_channels: int,
+        kernel_size: Union[int, tuple[int, int]] = 3,
+        stride: Union[int, tuple[int, int]] = 1,
+        padding: Optional[Union[int, tuple[int, int], str]] = None,
+        groups: int = 1,
+        norm_layer: Optional[Callable[..., nn.Module]] = nn.BatchNorm2d,
+        activation_layer: Optional[Callable[..., nn.Module]] = nn.ReLU,
+        activation_params: Optional[dict] = {},
+        dilation: Union[int, tuple[int, int]] = 1,
+        inplace: Optional[bool] = True,
+        bias: Optional[bool] = None,
+    ) -> None:
+
+        super().__init__(
+            in_channels,
+            out_channels,
+            kernel_size,
+            stride,
+            padding,
+            groups,
+            norm_layer,
+            activation_layer,
+            activation_params,
+            dilation,
+            inplace,
+            bias,
+            nn.ConvTranspose2d,
+        )
