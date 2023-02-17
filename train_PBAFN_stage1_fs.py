97327e122ae88983f3b309d48896211601a170e7
Zero-nnkn <nnkhoinguyen9@gmail.com>
2023-02-17 21:32:28 +0700

    update pbafn stage 1

--- train_PBAFN_stage1_fs.py
+++ train_PBAFN_stage1_fs.py
@@ -23,17 +23,18 @@ os.makedirs(path,exist_ok=True)
 os.makedirs(opt.checkpoints_dir,exist_ok=True)
 
 
-def CreateDataset(opt):
+def CreateDataset(opt, phase='train'):
     #training with augumentation
     #from data.aligned_dataset import AlignedDataset_aug
     #dataset = AlignedDataset_aug()
     from data.aligned_dataset import AlignedDataset
     dataset = AlignedDataset()
     print("dataset [%s] was created" % (dataset.name()))
-    dataset.initialize(opt)
+    val = True if phase=='val' else False
+    dataset.initialize(opt, val=val)
     return dataset
 
-os.makedirs('sample_fs',exist_ok=True)
+os.makedirs('sample',exist_ok=True)
 iter_path = os.path.join(opt.checkpoints_dir, opt.name, 'iter.txt')
 
 torch.cuda.set_device(opt.gpu_ids[0])
@@ -48,12 +49,17 @@ start_epoch, epoch_iter = 1, 0
 train_data = CreateDataset(opt) 
 train_sampler = DistributedSampler(train_data)
 train_loader = DataLoader(train_data, batch_size=opt.batchSize, shuffle=False,
-                                               num_workers=4, pin_memory=True, sampler=train_sampler)
+                                               num_workers=8, pin_memory=True, sampler=train_sampler)
+
+val_data = CreateDataset(opt, phase='val') 
+val_sampler = DistributedSampler(train_data)
+val_loader = DataLoader(train_data, batch_size=opt.batchSize, shuffle=False,
+                                               num_workers=16, pin_memory=True, sampler=train_sampler)
+
+
 dataset_size = len(train_loader)
-print('#training images = %d' % dataset_size)
 
 warp_model = AFWM(opt, 45)
-warp_model.train()
 warp_model.cuda()
 warp_model = torch.nn.SyncBatchNorm.convert_sync_batchnorm(warp_model).to(device)
 
@@ -73,14 +79,16 @@ step_per_batch = dataset_size
 if opt.local_rank == 0:
     writer = SummaryWriter(path)
 
+best_val_loss = 100
 all_steps = dataset_size * (opt.niter + opt.niter_decay + 1 - start_epoch)
 for epoch in range(start_epoch, opt.niter + opt.niter_decay + 1):
     epoch_start_time = time.time()
     if epoch != start_epoch:
         epoch_iter = epoch_iter % dataset_size
 
+    warp_model.train()
+    train_loss = 0
     train_sampler.set_epoch(epoch)
-
     for i, data in enumerate(train_loader):
         iter_start_time = time.time()
 
@@ -135,18 +143,20 @@ for epoch in range(start_epoch, opt.niter + opt.niter_decay + 1):
             loss_all = loss_all + (num+1) * loss_l1 + (num + 1) * 0.2 * loss_vgg + (num+1) * 2 * loss_edge + (num + 1) * 6 * loss_second_smooth
 
         loss_all = 0.01 * loss_smooth + loss_all
+        train_loss += loss_all
 
-        if opt.local_rank == 0:
-          writer.add_scalar('loss_all', loss_all, step)
+        # if opt.local_rank == 0:
+        #   writer.add_scalar('loss_all', loss_all, step)
 
         optimizer_warp.zero_grad()
         loss_all.backward()
         optimizer_warp.step()
-        ############## Display results and errors ##########
 
-        path = 'sample_fs/'+opt.name
+        ############## Display results and errors ##########
+        path = 'sample/'+opt.name
         os.makedirs(path,exist_ok=True)
         if step % 1000 == 0:
+          # Tensorboard
           if opt.local_rank == 0:
             a = real_image.float().cuda()
             b = person_clothes.cuda()
@@ -159,7 +169,7 @@ for epoch in range(start_epoch, opt.niter + opt.niter_decay + 1):
             writer.add_image('combine', (combine.data + 1) / 2.0, step)
             rgb=(cv_img*255).astype(np.uint8)
             bgr=cv2.cvtColor(rgb,cv2.COLOR_RGB2BGR)
-            cv2.imwrite('sample_fs/'+opt.name+'/'+str(step)+'.jpg',bgr)
+            cv2.imwrite('sample/'+opt.name+'/'+str(step)+'.jpg',bgr)
 
         step += 1
         iter_end_time = time.time()
@@ -177,17 +187,83 @@ for epoch in range(start_epoch, opt.niter + opt.niter_decay + 1):
 
         if epoch_iter >= dataset_size:
             break
-       
+    
+    # Visualize train loss
+    train_loss /= len(train_loader)
+    writer.add_scalar('train_loss', train_loss, epoch)
+    ####################### Validate ##########################
+    warp_model.eval()
+    val_loss = 0
+    with torch.no_grad():
+      for i, data in enumerate(val_loader):
+          t_mask = torch.FloatTensor((data['label'].cpu().numpy()==7).astype(np.float64))
+          data['label'] = data['label']*(1-t_mask)+t_mask*4
+          edge = data['edge']
+          pre_clothes_edge = torch.FloatTensor((edge.detach().numpy() > 0.5).astype(np.int64))
+          clothes = data['color']
+          clothes = clothes * pre_clothes_edge
+          person_clothes_edge = torch.FloatTensor((data['label'].cpu().numpy()==4).astype(np.int64))
+          real_image = data['image']
+          person_clothes = real_image * person_clothes_edge
+          pose = data['pose']
+          size = data['label'].size()
+          oneHot_size1 = (size[0], 25, size[2], size[3])
+          densepose = torch.cuda.FloatTensor(torch.Size(oneHot_size1)).zero_()
+          densepose = densepose.scatter_(1,data['densepose'].data.long().cuda(),1.0)
+          densepose_fore = data['densepose']/24.0
+          face_mask = torch.FloatTensor((data['label'].cpu().numpy()==1).astype(np.int64)) + torch.FloatTensor((data['label'].cpu().numpy()==12).astype(np.int64))
+          other_clothes_mask = torch.FloatTensor((data['label'].cpu().numpy()==5).astype(np.int64)) + torch.FloatTensor((data['label'].cpu().numpy()==6).astype(np.int64)) + \
+                              torch.FloatTensor((data['label'].cpu().numpy()==8).astype(np.int64)) + torch.FloatTensor((data['label'].cpu().numpy()==9).astype(np.int64)) + \
+                              torch.FloatTensor((data['label'].cpu().numpy()==10).astype(np.int64))
+          preserve_mask = torch.cat([face_mask,other_clothes_mask],1)
+          concat = torch.cat([preserve_mask.cuda(),densepose,pose.cuda()],1)
+
+          #import ipdb; ipdb.set_trace()
+
+          flow_out = model(concat.cuda(), clothes.cuda(), pre_clothes_edge.cuda())
+          warped_cloth, last_flow, _1, _2, delta_list, x_all, x_edge_all, delta_x_all, delta_y_all = flow_out
+          warped_prod_edge = x_edge_all[4]
+
+          epsilon = 0.001
+          loss_smooth = sum([TVLoss(x) for x in delta_list])
+          loss_all = 0
+
+          for num in range(5):
+              cur_person_clothes = F.interpolate(person_clothes, scale_factor=0.5**(4-num), mode='bilinear')
+              cur_person_clothes_edge = F.interpolate(person_clothes_edge, scale_factor=0.5**(4-num), mode='bilinear')
+              loss_l1 = criterionL1(x_all[num], cur_person_clothes.cuda())
+              loss_vgg = criterionVGG(x_all[num], cur_person_clothes.cuda())
+              loss_edge = criterionL1(x_edge_all[num], cur_person_clothes_edge.cuda())
+              b,c,h,w = delta_x_all[num].shape
+              loss_flow_x = (delta_x_all[num].pow(2)+ epsilon*epsilon).pow(0.45)
+              loss_flow_x = torch.sum(loss_flow_x)/(b*c*h*w)
+              loss_flow_y = (delta_y_all[num].pow(2)+ epsilon*epsilon).pow(0.45)
+              loss_flow_y = torch.sum(loss_flow_y)/(b*c*h*w)
+              loss_second_smooth = loss_flow_x + loss_flow_y
+              loss_all = loss_all + (num+1) * loss_l1 + (num + 1) * 0.2 * loss_vgg + (num+1) * 2 * loss_edge + (num + 1) * 6 * loss_second_smooth
+
+          loss_all = 0.01 * loss_smooth + loss_all
+          val_loss += loss_all
+
+    val_loss /= len(val_loader)
+    writer.add_scalar('val_loss', val_loss, epoch)
+
+    if val_loss < best_val_loss:
+        best_val_loss = val_loss
+        save_checkpoint(model.module, os.path.join(opt.checkpoints_dir, opt.name, 'PBAFN_warp_best.pth'))
+        print('Saving best at epoch %d' % epoch)  
+    #######################
+
     # end of epoch 
     iter_end_time = time.time()
     if opt.local_rank == 0:
-      print('End of epoch %d / %d \t Time Taken: %d sec' %
-            (epoch, opt.niter + opt.niter_decay, time.time() - epoch_start_time))
+      print('End of epoch %d / %d: train_loss: %.3f \t val_loss: %.3f \t time: %d sec' %
+            (epoch, opt.niter + opt.niter_decay, train_loss, val_loss, time.time() - epoch_start_time))
 
     ### save model for this epoch
     if epoch % opt.save_epoch_freq == 0:
       if opt.local_rank == 0:
-        print('saving the model at the end of epoch %d, iters %d' % (epoch, total_steps))        
+        print('Saving the model at the end of epoch %d, iters %d' % (epoch, total_steps))        
         save_checkpoint(model.module, os.path.join(opt.checkpoints_dir, opt.name, 'PBAFN_warp_epoch_%03d.pth' % (epoch+1)))
 
     if epoch > opt.niter:
